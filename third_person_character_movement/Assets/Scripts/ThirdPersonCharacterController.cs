680b0d40f73cdb0fb0390cc35ae9703941461eda
Dylan <dmalla@live.com>
2020-01-30 22:37:06 -0800

    Created scene layout and player. Implemented player and 3rd person camera movement.

---
+++ third_person_character_movement/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(horizontalMove, 0f, verticalMove) * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
